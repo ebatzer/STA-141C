19118aa2d0f7587f84e33c0b089ab6e4de43e05e
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-11 15:56:27 -0800

    Update hw4shell.sh

--- hw4shell.sh
+++ hw4shell.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition staclass
 
 # Give the job a name
-#SBATCH --job-name hw4
+#SBATCH --job-name submission
 
 # Setting DATAFILE
 unzip -p ../../group/staclassgrp/transaction.zip | cat > transaction_large
@@ -21,7 +21,7 @@ grep -n -i bicycle transaction_large > bicycle.csv
 
 # Finding the list of unique funding agency ID's
 # First remove the first row (no column IDs)
-tail -n+2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
+tail -n +2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
 
 # Find the description and amount
 cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
