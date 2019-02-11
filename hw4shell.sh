cd9028c42f287396dd2a7512e8c8edb74c77f949
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-11 15:51:58 -0800

    Update hw4shell.sh

--- hw4shell.sh
+++ hw4shell.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -l
 
 # Use the staclass partition. Only applies if you are in STA141C
-#sbatch --partition = staclass
+#SBATCH --partition staclass
 
 # Give the job a name
-#sbatch --job-name = "bashtest"
+#SBATCH --job-name bashtest
 
 # Setting DATAFILE
 unzip -p ../../group/staclassgrp/transaction.zip | cat > transaction_large
+
+# For indices of column names
+head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
 
 # For length of largest line
 wc -L transaction_large > maxchars.txt
 
 # For number of lines where bicycle appears
+# -i specifies case insensitive
 grep -n -i bicycle transaction_large > bicycle.csv
 
 # Finding the list of unique funding agency ID's
-cut -f 18 -d , transaction_large | sort | uniq > funding_agency_set.txt
+# First remove the first row (no column IDs)
+tail -n + 2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
+
+# Find the description and amount
+cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
