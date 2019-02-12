5d4aea85da83376707fe337b022024878b16f748
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-11 16:04:35 -0800

    Update hw4shell.sh

--- hw4shell.sh
+++ hw4shell.sh
@@ -4,24 +4,24 @@
 #SBATCH --partition staclass
 
 # Give the job a name
-#SBATCH --job-name sub
+#SBATCH --job-name hwsub
 
 # Setting DATAFILE
-unzip -p ../../group/staclassgrp/transaction.zip | cat > transaction_large
+unzip -p ../../group/staclassgrp/transaction.zip > transaction_large
 
 # For indices of column names
-# head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
+head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
 
 # For length of largest line
-# wc -L transaction_large > maxchars.txt
+wc -L transaction_large > maxchars.txt
 
 # For number of lines where bicycle appears
 # -i specifies case insensitive
-# grep -n -i bicycle transaction_large > bicycle.csv
+grep -n -i bicycle transaction_large > bicycle.csv
 
 # Finding the list of unique funding agency ID's
 # First remove the first row (no column IDs)
-# tail -n +2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
+tail -n +2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
 
 # Find the description and amount
-# cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
+cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
