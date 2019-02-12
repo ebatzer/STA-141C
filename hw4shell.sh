d18b7b1cd8ed4a6618acc70eb45240000901e5f2
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-11 21:11:09 -0800

    Final HW4 Update

--- hw4shell.sh
+++ hw4shell.sh
@@ -7,7 +7,7 @@
 #SBATCH --job-name hwsub
 
 # Unzipping transaction file (commented out after running 1st time)
-# unzip -p ../../group/staclassgrp/transaction.zip > transaction_large
+unzip -p transaction.zip > transaction_large
 
 # For indices of column names
 head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
@@ -16,12 +16,33 @@ head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
 wc -L transaction_large > maxchars.txt
 
 # For number of lines where bicycle appears
-# -i specifies case insensitive
+# -i specifies case insels nsitive
 grep -n -i bicycle transaction_large > bicycle.csv
 
 # Finding the list of unique funding agency ID's
 # First remove the first row (no column IDs)
-tail -n +2 transaction_large | cut -f 18 -d , | sort | uniq > funding_agency_set.txt
+tail -n +2 transaction_large | cut -f 18,54 -d , | sort | uniq > funding_agency_set.txt
 
 # Find the description and amount
 cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
+
+
+# Finding where border funding goes
+# First, cut down dataset to relevant columns
+# In this case: obligation, description, and recipient state ID
+cut -f 8,25,36 -d , transaction_large |
+
+# Then, search for the term "border" in the funding description (case insensitive)
+grep -i border |
+
+# Then, using awk, delimit fields by "," and sum over unique values of the array
+# Source from https://stackoverflow.com/questions/10286522/group-by-sum-from-shell
+awk -F '[,]' '{
+    arr[$3]+=$1
+   }
+   END {
+     for (key in arr) printf("%s\t%s\n", key, arr[key])
+   }'|
+
+# Sort over the first column and output a new text document
+sort -k1,1 > borderfunding.txt
