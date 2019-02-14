d5e800f9eb7fdf9bbacbb415817ea7ef6e07e1b0
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-14 14:46:03 -0800

    Updates in response to grading rubric

--- hw4shell.sh
+++ hw4shell.sh
@@ -6,43 +6,60 @@
 # Give the job a name
 #SBATCH --job-name hwsub
 
-# Unzipping transaction file
-unzip -p ../../scratch/transaction.zip > transaction_large
+# Global Variables
+DATAFILE = "../../scratch/transaction.zip"
+AMT = 8 # Funding amount
+FUNDID = 18 # Funding agency ID
+DESC = 25 # Payment description
+STATEID = 36 # Recipient state ID
+FUNDDESC = 54 # Funding agency description (text)
 
 # For indices of column names
-head -n 1 transaction_large | tr -s "," "\n" | nl > column_index.txt
+unzip -p ${DATAFILE} |
+	head --lines 1 |
+	tr --squeeze-repeats "," "\n" |
+	nl > column_index.txt
 
 # For length of largest line
-wc -L transaction_large > maxchars.txt
+unzip -p ${DATAFILE} |
+	wc --max-line-length > maxchars.txt
 
 # For number of lines where bicycle appears
-# -i specifies case insels nsitive
-grep -n -i bicycle transaction_large > bicycle.csv
+unzip -p ${DATAFILE} |
+	grep -n --ignore-case bicycle > bicycle.csv
 
-# Finding the list of unique funding agency ID's
+# Finding the list of unique funding agency ID's and funding agency descriptions
 # First remove the first row (no column IDs)
-tail -n +2 transaction_large | cut -f 18,54 -d , | sort | uniq > funding_agency_set.txt
+unzip -p ${DATAFILE} |
+	tail --lines +2 |
+	cut --fields ${FUNDID},${FUNDDESC} --delimiter , |
+	sort |
+	uniq > funding_agency_set.txt
 
 # Find the description and amount
-cut -f 8,25 -d , transaction_large | sort -k1 -r -n | head -n 3 > largest.csv
-
+unzip -p ${DATAFILE} |
+	cut --fields ${AMT},${DESC} -d , |
+	sort -k1 --reverse --numeric-sort |
+	head -n 3 > largest.csv
 
 # Finding where border funding goes
 # First, cut down dataset to relevant columns
 # In this case: obligation, description, and recipient state ID
-cut -f 8,25,36 -d , transaction_large |
+unzip -p ${DATAFILE} |
+	cut --fields ${AMT},${DESC},${STATEID} --delimiter , |
 
 # Then, search for the term "border" in the funding description (case insensitive)
-grep -i border |
+	grep -i border |
 
-# Then, using awk, delimit fields by "," and sum over unique values of the array
-# Source from https://stackoverflow.com/questions/10286522/group-by-sum-from-shell
-awk -F '[,]' '{
-    arr[$3]+=$1
-   }
-   END {
-     for (key in arr) printf("%s\t%s\n", key, arr[key])
-   }'|
+	# Then, using awk, delimit fields by "," and sum over unique values of the array
+	# Unique values of the third column have summed values in the first column
+	# Source from https://stackoverflow.com/questions/10286522/group-by-sum-from-shell
+	awk -F '[,]' '{
+	    arr[$3]+=$1
+	   }
+	   END {
+	     for (key in arr) printf("%s\t%s\n", key, arr[key])
+	   }'|
 
-# Sort over the first column and output a new text document
-sort -k1,1 > borderfunding.txt
+	# Sort over the first column and output a new text document
+	sort -k1,1 > borderfunding.txt
