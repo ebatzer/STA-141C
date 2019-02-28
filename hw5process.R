6fcbdcfde254928270db75edaa7772e2d5352868
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-28 12:54:55 -0800

    Update hw5process.R

--- hw5process.R
+++ hw5process.R
@@ -5,4 +5,4 @@ d_filtered = d[d$digit %in% c(1:9),]
 d_filtered = d_filtered[,c(2,3)]
 counts = table(d_filtered)
 
-write.csv(x = counts, "digit_counts.csv")
+write.csv(x = as.data.frame.matrix(counts), "digit_counts.csv")
