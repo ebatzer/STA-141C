3a6022dfc52534140847cf0fb0b2004af1eafcd7
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-28 19:07:13 -0800

    Edit process (transpose locally)

--- hw5kld.R
+++ hw5kld.R
@@ -1,2 +1,17 @@
-dir()
+
+digits = read.csv("digit_counts.csv", header = TRUE)
+head(digits)
+
+BenFord <- function(x){
+  log10(1 + (1/x))
+}
+
+sum(BenFord(1:9))
+
+Uniform <- function(){
+  (1 / 9)
+}
+
+Uniform()
+
 
