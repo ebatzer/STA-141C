aaf6d61924c50586e404f1368431b8162711facc
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-28 19:36:28 -0800

    Checking counting function

--- hw5kld.R
+++ hw5kld.R
@@ -1,6 +1,18 @@
+library(tidyverse)
 
+# Reading in digit counts
 digits = read.csv("digit_counts.csv", header = TRUE)
-head(digits)
+
+# Transposing dataframe from long to wide
+digit_wide = digits %>% 
+  select(-X) %>% 
+  spread(key = "digit", value = "Freq")
+
+cont_table = digit_wide[rowSums(digit_wide[,2:10]) > 100,]
+
+obs_freq = colSums(cont_table[-1,2:10])
+
+colnames(digits) = c("Agency", c(1:9))
 
 BenFord <- function(x){
   log10(1 + (1/x))
