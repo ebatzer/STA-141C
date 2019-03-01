56486afacc8ed30ec6f43319be5445690000a1fd
Evan Batzer <ebatzer@ucdavis.edu>
2019-02-28 18:50:09 -0800

    Transposing digit counts

--- hw5process.R
+++ hw5process.R
@@ -1,8 +1,9 @@
 d = read.table("digits.txt", sep = ",", header = TRUE)
 colnames(d) = c("action_date", "digit", "recipient")
+d$digit = as.numeric(d$digit)
 
 d_filtered = d[d$digit %in% c(1:9),]
 d_filtered = d_filtered[,c(2,3)]
 counts = table(d_filtered)
 
-write.csv(x = as.data.frame.matrix(counts), "digit_counts.csv")
+write.csv(x = t(as.data.frame.matrix(counts)), "digit_counts.csv")
